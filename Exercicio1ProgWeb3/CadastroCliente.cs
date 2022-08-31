2f3b703df14d9e9359454a1ea0fa262c3aa1f99e
Paulo Ricardo Stradioti <paulo@paulo.eti.br>
2022-08-31 19:26:05 -0300

    Sugestões de alterações para usar o tipo anulável de DateTime.

--- Exercicio1ProgWeb3/CadastroCliente.cs
+++ Exercicio1ProgWeb3/CadastroCliente.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Win32.SafeHandles;
 
 namespace Exercicio1ProgWeb3
 {
@@ -12,20 +13,21 @@ namespace Exercicio1ProgWeb3
         [StringLength(11, MinimumLength = 11, ErrorMessage = "O CPF deve ter 11 números")]
         public string? Cpf { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "A Data de Nascimento é Obrigatória")]
         [DataType(DataType.Date)]
-        public DateTime Nascimento { get; set; }
+        public DateTime? Nascimento { get; set; }
 
-        [Range (18, int.MaxValue, ErrorMessage = "Você deve ter mais de 18 anos para se cadastrar")]
-        public int Idade { get { return CalcularIdade(); } }
+        [Range(18, int.MaxValue, ErrorMessage = "Você deve ter mais de 18 anos para se cadastrar")]
+        public int? Idade { get { return CalcularIdade(); } }
 
         public int CalcularIdade()
         {
-            int idade = DateTime.Now.Year - Nascimento.Year;
-            if (DateTime.Now.DayOfYear > Nascimento.DayOfYear)
-            {
+            int idade = Nascimento.HasValue ? DateTime.Now.Year - Nascimento.Value.Year : 0;
+            
+            if (Nascimento.HasValue && 
+                DateTime.Now.DayOfYear < Nascimento.Value.DayOfYear)
                 idade--;
-            }
+
             return idade;
         }
     }
